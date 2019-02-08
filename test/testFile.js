b688ebcde0a45460d65faee001784a38a0235091
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-08 12:29:22 -0800

    added test case for adding to list

--- test/testFile.js
+++ test/testFile.js
@@ -12,4 +12,10 @@ describe('store', function () {
       assert.deepStrictEqual(store.getSortedList(), [1, 2, 3, 4, 5]);
     });
   });
+  describe('#addToList()', function () {
+    it('should return a new Array containing the new addition along with everything else in secureStorage', function () {
+      store.addToList(6)
+      assert.deepStrictEqual(store.getList(), [5, 3, 2, 1, 4, 6]);
+    });
+  });
 });
